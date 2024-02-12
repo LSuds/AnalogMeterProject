e27e451f6a8f5a68d2fc96dfcbb6cd6575be757a
luke sudtelgte <luke31500@gmail.com>
2024-02-11 22:16:40 -0600

    added system stats to json

--- AnalogMeterProject/AnalogMeterProject.py
+++ AnalogMeterProject/AnalogMeterProject.py
@@ -1,8 +1,8 @@
-# GPU information
 import GPUtil
 import time 
 import socket
-
+import psutil
+import json
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 12000))
@@ -10,12 +10,36 @@ server_socket.bind(('', 12000))
 message, address = server_socket.recvfrom(1024)
 
 while(1):
-    gpu = GPUtil.getGPUs()[0]
-    gpu_usage = round(gpu.load*100)
-    gpu_mem_usage = round(gpu.memoryUtil*100)
+    Gpus = GPUtil.getGPUs()
+    CpuCores = psutil.cpu_percent(interval=1, percpu=True) 
 
-    message = f'{gpu_usage} {gpu_mem_usage}'.encode()
-    print(f'Sending GPU Usage and GPU Mem Usage. {gpu_usage} : {gpu_mem_usage}')
-    server_socket.sendto(message, address)
+    systemStatJson = {
+        "CpuStat":[],
+        "GpuStat":[],
+        "MemStat":[]
+    }
 
-    time.sleep(1)
+    #FETCH CPU CORE STATS AND ADD TO JSON
+    print('CPU Stats:')
+    for cpu in CpuCores:
+        roundedCoreUtil = round(cpu)
+        systemStatJson["CpuStat"].append(roundedCoreUtil)
+
+    #FETCH GPU STATS AND ADD TO JSON
+    for gpu in Gpus:
+        gpu_info = {
+            "Gpu_Usage": round(gpu.load*100),
+            "Gpu_Mem_Usage": round(gpu.memoryUtil*100)
+        }
+        systemStatJson["GpuStat"].append(gpu_info)
+
+    #FETCH MEMORY UTIL AND ADD TO JSON
+    roundedMemPercentUsage = round(psutil.virtual_memory()[2])
+    systemStatJson["MemStat"].append(roundedMemPercentUsage)
+
+
+    message = json.dumps(systemStatJson, indent=4)
+    server_socket.sendto(message.encode(), address)
+    print(systemStatJson)
+
+    time.sleep(10)
