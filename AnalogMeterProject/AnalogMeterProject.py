5acc4562c9ac39f64443baa3b11e6e219c364652
luke sudtelgte <luke31500@gmail.com>
2024-02-16 16:00:03 -0600

    updated names

--- AnalogMeterProject/AnalogMeterProject.py
+++ AnalogMeterProject/AnalogMeterProject.py
@@ -8,24 +8,23 @@ import json
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 12000))
 
-message, address = server_socket.recvfrom(1024)
-
 while(1):
+    message, address = server_socket.recvfrom(1024)
     Gpus = GPUtil.getGPUs()
     CpuCores = psutil.cpu_percent(interval=1, percpu=True) 
     MemSwap = psutil.swap_memory()
 
     systemStatJson = {
-        "CpuStat":[],
-        "GpuStat":[],
-        "MemStat":[],
-        "StorageStat":[]
+        "Cpu":[],
+        "Gpu":[],
+        "Mem":[],
+        "Storage":[]
     }
 
     #FETCH CPU CORE STATS AND ADD TO JSON
     for cpu in CpuCores:
         roundedCoreUtil = round(cpu)
-        systemStatJson["CpuStat"].append(roundedCoreUtil)
+        systemStatJson["Cpu"].append(roundedCoreUtil)
 
     #FETCH GPU STATS AND ADD TO JSON
     for gpu in Gpus:
@@ -33,23 +32,23 @@ while(1):
             "Gpu_Usage": round(gpu.load*100),
             "Gpu_Mem_Usage": round(gpu.memoryUtil*100)
         }
-        systemStatJson["GpuStat"].append(gpu_info)
+        systemStatJson["Gpu"].append(gpu_info)
 
     #FETCH MEMORY UTIL AND ADD TO JSON
     mem_info = {
         "Mem_Percent_Usage": round(psutil.virtual_memory()[2]),
         "Mem_Swap": round(MemSwap.percent)
     }
-    systemStatJson["MemStat"].append(mem_info)
+    systemStatJson["Mem"].append(mem_info)
 
     #FETCH DISK STATS AND ADD TO JSON
     for part in psutil.disk_partitions(all=False):
         usage = psutil.disk_usage(part.mountpoint)
-        systemStatJson["StorageStat"].append(round(usage.percent))
+        systemStatJson["Storage"].append(round(usage.percent))
 
     #ENCODE JSON AND SEND TO CLIENT
     message = json.dumps(systemStatJson, indent=4)
     server_socket.sendto(message.encode(), address)
     print(systemStatJson)
 
-    time.sleep(10)
+    time.sleep(.1)
